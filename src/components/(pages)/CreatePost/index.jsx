bc45a960396dc8452d2295bfdc14be85413a029f
Juan <juan.gauna.dev@gmail.com>
2024-03-18 02:22:06 -0300

    feat: add styles

--- src/components/(pages)/CreatePost/index.jsx
+++ src/components/(pages)/CreatePost/index.jsx
@@ -48,7 +48,7 @@ const CreatePost = () => {
           toast.success("Success");
           setTimeout(() => {
             navigate("/");
-          }, 2000);
+          }, 1000);
           return res;
         }
       } catch (err) {
@@ -65,35 +65,18 @@ const CreatePost = () => {
   };
   return (
     <section className="max-w-6xl mx-auto">
-      <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Generate your Image
+        </h1>
         <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
-          Generate an imaginative image through DALL-E AI and share it with the
-          community
+          Generate an imaginative image and share it with the community
         </p>
       </div>
 
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+      <form className="m-auto mt-16 max-w-2xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
-          <FormField
-            labelName="Your Name"
-            type="text"
-            name="name"
-            placeholder="Ex., john doe"
-            value={form.name}
-            handleChange={handleChange}
-          />
-
-          <FormField
-            labelName="Image Description"
-            type="text"
-            name="prompt"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
-            value={form.prompt}
-            handleChange={handleChange}
-          />
-
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          <div className="m-auto relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             {form.photo ? (
               <img
                 src={form.photo}
@@ -114,29 +97,51 @@ const CreatePost = () => {
               </div>
             )}
           </div>
+          <div className="m-auto mt-5 flex gap-5">
+            <button
+              type="button"
+              onClick={generateImage}
+              className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {generatingImg ? "Generating..." : "Generate"}
+            </button>
+          </div>
+          {form?.photo && (
+            <>
+              <FormField
+                labelName="Image Description"
+                type="text"
+                name="prompt"
+                placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+                value={form.prompt}
+                handleChange={handleChange}
+              />
+              <FormField
+                labelName="Your Name"
+                type="text"
+                name="name"
+                placeholder="Ex., john doe"
+                value={form.name}
+                handleChange={handleChange}
+              />
+            </>
+          )}
         </div>
-        <div className="mt-5 flex gap-5">
-          <button
-            type="button"
-            onClick={generateImage}
-            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {generatingImg ? "Generating..." : "Generate"}
-          </button>
-        </div>
+        {form?.photo && (
+          <div className="m-auto mt-10 flex flex-col items-center">
+            <p className="mt-2 text-[#666e75] text-[14px]">
+              ** Once you have created the image you want, you can share it with
+              others in the community **
+            </p>
+            <button
+              type="submit"
+              className=" mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {loading ? "Sharing..." : "Share with the Community"}
+            </button>
+          </div>
+        )}
 
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">
-            ** Once you have created the image you want, you can share it with
-            others in the community **
-          </p>
-          <button
-            type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {loading ? "Sharing..." : "Share with the Community"}
-          </button>
-        </div>
         <Toaster />
       </form>
     </section>
--- src/components/(subcomponents)/Header/index.jsx
+++ src/components/(subcomponents)/Header/index.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import logo from "../../../assets/logo.svg";
+import logo from "../../../assets/logo.png";
 
 function Header() {
   return (
     <header className="w-full max-w-7xl mx-auto flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to={"/"}>
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={logo} alt="logo" className="w-72 object-contain" />
       </Link>
       <Link
         to={"/create-post"}
-        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mr-10"
       >
         Create
       </Link>
